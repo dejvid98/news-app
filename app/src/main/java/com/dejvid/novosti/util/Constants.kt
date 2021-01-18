9d31bd32f18dbc1788fa606662c8f249ea777dfe
David Sucur <52608605+dejvid98@users.noreply.github.com>
2021-01-18 20:09:28 +0100

    API Key update

--- app/src/main/java/com/dejvid/novosti/util/Constants.kt
+++ app/src/main/java/com/dejvid/novosti/util/Constants.kt
@@ -2,7 +2,7 @@ package com.dejvid.novosti.util
 
 class Constants {
     companion object {
-        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val API_KEY = "4e54a09932c9456f9914180dd2a4cfcb"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
